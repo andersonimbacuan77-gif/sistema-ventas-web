6de32af93600cfd279112cd80bbe8cdbb27e705f
andersonimbacuan77-gif <andersonimbacuan77@gmail.com>
2026-03-19 08:29:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
 
 // Middleware
 app.use(cors());
